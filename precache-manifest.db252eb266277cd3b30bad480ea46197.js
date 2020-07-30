543cf225969b3f89c12cf004f706852596eed7f5
Михайлов Артём Андреевич <amihaylov@ask.local>
2020-07-30 16:49:42 +0700

    Updates

---
+++ precache-manifest.db252eb266277cd3b30bad480ea46197.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abc053f71467272285564ac99ded384d",
+    "url": "/table-test/index.html"
+  },
+  {
+    "revision": "32dc84e302088ce61a49",
+    "url": "/table-test/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "7f34408c52cb08e7832b",
+    "url": "/table-test/static/css/main.ead857f1.chunk.css"
+  },
+  {
+    "revision": "32dc84e302088ce61a49",
+    "url": "/table-test/static/js/2.732e35ba.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/table-test/static/js/2.732e35ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f34408c52cb08e7832b",
+    "url": "/table-test/static/js/main.b9f491cb.chunk.js"
+  },
+  {
+    "revision": "0406e3ad428b6872a907",
+    "url": "/table-test/static/js/runtime-main.d9a0b2fa.js"
+  }
+]);
